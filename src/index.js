812f322c23de8614625db2fd38be6bbcaf7cf917
Christoph Michel <MrToph@users.noreply.github.com>
2017-09-24 18:08:46 +0200

    Remove useless dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 const HeadlessChrome = require("simple-headless-chrome");
-const chromeLauncher = require("chrome-launcher");
-const CDP = require("chrome-remote-interface");
 const fs = require("fs");
 const scrapeRescueTime = require("./rescueTime");
 const scrapeAdSense = require("./adSense");
 const scrapeAnalytics = require("./analytics");
 
 const config = require("./config/config.json");
-
-const { Chromeless } = require("chromeless");
 
 const browser = new HeadlessChrome({
   headless: false,
